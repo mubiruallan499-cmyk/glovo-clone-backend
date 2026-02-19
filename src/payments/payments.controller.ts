6d6d99848c6faf3c4933645542f032f349e46037
mubiruallan499-cmyk <mubiruallan499@gmail.com>
2026-02-19 17:40:26 +0300

    Update payments.controller.ts

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,11 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+
+import { Controller, Post, Body } from '@nestjs/common';
+import { PaymentsService } from './payments.service';
+import { Payment } from './payment.entity';
 
 @Controller('payments')
 export class PaymentsController {
-  @Get()
-  findAll() {
-    return {
-      message: 'Payments endpoint working',
-    };
+  constructor(private readonly paymentsService: PaymentsService) {}
+
+  @Post()
+  async create(@Body() body: { userId: number; amount: number; method: string }) {
+    try {
+      const payment = await this.paymentsService.create(body);
+      return { message: 'payment created', data: payment };
+    } catch (error) {
+      console.error(error); // Important for debugging
+      throw error;
+    }
   }
 }
