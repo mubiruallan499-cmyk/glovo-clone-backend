86c777df71a565b0d9373933e7744623d3947c15
mubiruallan499-cmyk <mubiruallan499@gmail.com>
2026-02-19 16:44:55 +0300

    Update payments.service.ts

--- src/payments/payments.service.ts
+++ src/payments/payments.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Payment } from './payment.entity';
 
 @Injectable()
-export class PaymentsService {}
+export class PaymentsService {
+  constructor(
+    @InjectRepository(Payment)
+    private readonly paymentRepository: Repository<Payment>,
+  ) {}
+
+  // Create a new payment
+  async create(data: {
+    userId: number;
+    amount: number;
+    method: string;
+  }): Promise<Payment> {
+    const payment = this.paymentRepository.create(data);
+    return this.paymentRepository.save(payment);
+  }
+
+  // Get all payments
+  async findAll(): Promise<Payment[]> {
+    return this.paymentRepository.find();
+  }
+
+  // Get a payment by ID
+  async findOne(id: number): Promise<Payment> {
+    return this.paymentRepository.findOneBy({ id });
+  }
+}
